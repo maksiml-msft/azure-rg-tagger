9f3812777c4d5545be541cd4b04c68bbd0334a88
Maksim Libenson <maksiml@microsoft.com>
2022-07-29 14:58:23 -0700

    import tag-owner code

--- rg/index.js
+++ rg/index.js
@@ -1,13 +1,55 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(req.body);
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.log.verbose("started");
+    var eventPayload = require('zealit')(req.body.data); //helps nested lookups by turning undefined gets to exceptions
+    context.log.verbose(eventPayload);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    //Apply filter that is not configurable from the alert creation
+    context.log.verbose("checking additional filter");
+    if (!shouldRespondToEvent(eventPayload)) {
+        context.log.verbose("event was filtered out");
+        end(200, "skipped", "verbose");
+        return;
+    }
+
+    context.log.verbose("extracting mandatory data from event");
+    try {
+        var rgName = eventPayload.context.activityLog.resourceGroupName;
+        var userName = eventPayload.context.activityLog.caller;
+        var subscriptionId = eventPayload.context.activityLog.subscriptionId;
+    }
+    catch (err) {
+        context.log.verbose("failed to extract mandatory data from event");
+        end(400, "error extracting data from body", "error");
+        return;
+    }
+
+    end(200, "done:" + rgName, "verbose");
+
+    function shouldRespondToEvent(eventPayload) {
+        try {
+            var subStatus = eventPayload.context.activityLog.subStatus;
+        }
+        catch (err) {
+            return false;
+        }
+
+        if (subStatus == "Created") {
+            return true;
+        }
+        return false;
+    }
+
+    function end(httpStatus, message, level) {
+        if (level == 'error') {
+            context.log.error(message);
+        }
+        if (level == 'verbose') {
+            context.log.verbose(message);
+        }
+        context.res = {
+            status: httpStatus,
+            body: message
+        };
+        context.done();
+    }
 }
